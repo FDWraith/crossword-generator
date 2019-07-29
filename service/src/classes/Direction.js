e4167208943cede026550bd8f54c1610d50564e2
FDWraith <kevinzhang3702@gmail.com>
2019-07-29 01:07:05 -0400

    Revert "moved files into src folder, start implementing tests"
    This reverts commit 50468c1d21e8af51e16c79e86e11f8944b1176ce.

--- service/src/classes/Direction.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Direction;
-(function (Direction) {
-    Direction["Vertical"] = "vertical";
-    Direction["Horizontal"] = "horizontal";
-})(Direction || (Direction = {}));
-exports["default"] = Direction;
